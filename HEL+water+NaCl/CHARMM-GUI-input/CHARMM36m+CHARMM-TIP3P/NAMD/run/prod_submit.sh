5a718cf47e67fc68a7dc63b7088a6d3e3492cfeb
Amruthesh Thirumalaiswamy <amruthesht@users.noreply.github.com>
2025-03-04 12:11:36 -0700

    Changes that determine and assign custom core list using taskset

--- HEL+water+NaCl/CHARMM-GUI-input/CHARMM36m+CHARMM-TIP3P/NAMD/run/prod_submit.sh
+++ HEL+water+NaCl/CHARMM-GUI-input/CHARMM36m+CHARMM-TIP3P/NAMD/run/prod_submit.sh
@@ -6,17 +6,18 @@
 #SBATCH --partition=general         # Specify partition
 #SBATCH --nodes=1                   # Use one node
 #SBATCH --ntasks=1                  # Run a single job
-#SBATCH --cpus-per-task=48          # Total CPU cores requested
+#SBATCH --cpus-per-task=42          # Total CPU cores requested
+# SBATCH --cpus-per-task=10          # Total CPU cores requested
 #SBATCH --gres=gpu:a100:1                # Request 1 GPU
-#SBATCH --time=22:00:00           # Max job time (22 hours)
-#SBATCH --exclusive                 # Get exclusive access to the node
+#SBATCH --time=24:00:00           # Max job time (24 hours)
+# SBATCH --exclusive                 # Get exclusive access to the node
 
 # Set number of cores
-TOTAL_CORES=48
-NAMD_CORES=40
-PYTHON_CORES=8
+TOTAL_CORES=42 # 48
+NAMD_CORES=40 # 40
+PYTHON_CORES=2 # 8
 GPU_DEVICE=0
-n_runs=3
+n_runs=1
 
 # Run NAMD equilibration
 ./run/equi.sh ${NAMD_CORES} ${GPU_DEVICE}
@@ -29,7 +30,7 @@ n_runs=3
 ./run/prod_imdv3.sh ${NAMD_CORES} ${GPU_DEVICE} ${n_runs}
 
 # IMDv3 NAMD with File I/O
-./run/prod_imdv3_fileio.sh ${NAMD_CORES} ${GPU_DEVICE} ${n_runs}
+./run/prod_fileio.sh ${NAMD_CORES} ${GPU_DEVICE} ${n_runs}
 
 # IMDv3 NAMD with streaming
-./run/prod_imdv3_streaming.sh ${NAMD_CORES} ${GPU_DEVICE} ${PYTHON_CORES} ${n_runs}
+./run/prod_streaming.sh ${NAMD_CORES} ${GPU_DEVICE} ${PYTHON_CORES} ${n_runs}
---
+++ HEL+water+NaCl/CHARMM-GUI-input/CHARMM36m+CHARMM-TIP3P/NAMD/run/test_submit.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#SBATCH --job-name=perf_test_NAMD      # Job name
+#SBATCH --output=perf_test_%j.out        # Output file
+#SBATCH --error=perf_test_%j.err         # Error file
+#SBATCH --partition=general              # Specify partition
+#SBATCH --nodes=1                        # Use one node
+#SBATCH --ntasks=1                       # Run a single job
+#SBATCH --cpus-per-task=48               # Total CPU cores requested
+#SBATCH --gres=gpu:a100:1                # Request 1 GPU
+#SBATCH --time=22:00:00                  # Max job time (24 hours)
+#SBATCH --exclusive                   # Optional: exclusive access if desired
+
+# Number of runs for each test
+n_runs=3
+
+# Set number of cores
+NAMD_CORES=40
+GPU_DEVICE=0
+# Run NAMD equilibration
+./run/equi.sh ${NAMD_CORES} ${GPU_DEVICE}
+
+# Loop over NAMD_CORES values [10,20,30,40]
+for NAMD_CORES in 6 10 12 20 24 30 36 40 48; do
+    GPU_DEVICE=0
+    PYTHON_CORES=2
+    echo "Running tests with NAMD_CORES=${NAMD_CORES}, GPU_DEVICE=${GPU_DEVICE}, PYTHON_CORES=${PYTHON_CORES}"
+    
+    # Run vanilla NAMD production
+    ./run/prod_vanilla.sh ${NAMD_CORES} ${GPU_DEVICE} ${n_runs}
+    
+    # Run IMDv3 NAMD production
+    ./run/prod_imdv3.sh ${NAMD_CORES} ${GPU_DEVICE} ${n_runs}
+    
+    # Run IMDv3 NAMD with File I/O production
+    # ./run/prod_fileio.sh ${NAMD_CORES} ${GPU_DEVICE} ${n_runs}
+    
+    # Run IMDv3 NAMD with Streaming production (with Python client)
+    # ./run/prod_streaming.sh ${NAMD_CORES} ${GPU_DEVICE} ${PYTHON_CORES} ${n_runs}
+done
